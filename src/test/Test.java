343ff11a439e29b0b157c63d197f368cdd726bba
vhh <vhh0106@gmail.com>
2021-10-18 15:21:48 +0700

    1.1..1

--- src/test/Test.java
+++ src/test/Test.java
@@ -21,3 +21,26 @@ public class Test {
     }
     
 }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author lizid
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tinh dep trai");
+        System.out.println("Tinh dep trai nua ne");
+        System.out.println("Hiep                              dep trai nua ne");
+    }
+    
+}
